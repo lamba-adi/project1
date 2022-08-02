d6373d589243e6c99a7d2c77b9dbca6a44c84e51
lamba-adi <106315699+lamba-adi@users.noreply.github.com>
2022-08-02 12:47:02 +0530

    Update employee.service.ts

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { employee } from './employee';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) { }
+
+  getByEmail(email:string){
+
+  }
 }
